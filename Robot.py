7169abbecceeed6cd47ca3e45f865b8cfd2d4466
Jakub Wieczorek <jwieczor@pantera.robotyka.ia.pw.edu.pl>
2016-10-21 12:05:28 +0200

    Works on lightness

--- Robot.py
+++ Robot.py
@@ -5,10 +5,9 @@ class Controller:
 		self.leftMotor = ev3.LargeMotor('outA')
 		self.rightMotor = ev3.LargeMotor('outD')
 		self.colorSensor = ev3.ColorSensor('in1')
+		self.colorSensor.mode = 'COL-REFLECT'
 
 	def goForward(self, leftSpeed = -400, rightSpeed = -400, time = None):
-		#ev3.Sound.speak('Go go go').wait()
-		
 		if time == None:	
 			self.rightMotor.run_forever(speed_sp = rightSpeed)
 			self.leftMotor.run_forever(speed_sp = leftSpeed)
@@ -16,12 +15,14 @@ class Controller:
 			self.leftMotor.run_timed(time_sp = 3000, speed_sp = leftSpeed)
 			self.rightMotor.run_timed(time_sp = 3000, speed_sp = rightSpeed)
 
-	def turnRight(self, speed = -800):
+	def turnRight(self, speed = -300):
 		self.stop()
+		self.rightMotor.run_forever(speed_sp = -speed)
 		self.leftMotor.run_forever(speed_sp = speed)
 	
-	def turnLeft(self, speed = -800):
+	def turnLeft(self, speed = -300):
 		self.stop()
+		self.leftMotor.run_forever(speed_sp = -speed)
 		self.rightMotor.run_forever(speed_sp = speed)
 			
 	def readColor(self):	
@@ -31,26 +32,6 @@ class Controller:
 		self.leftMotor.stop()
 		self.rightMotor.stop()
 
-	def showDetectedColor(self):
-       		color = self.readColor()
-
-        	if color == 0:
-                	print("None")
-        	elif color == 1:
-               		print("Black")
-        	elif color == 2:
-               		print("Blue")
-       		elif color == 3:
-               		print("Green")
-       		elif color == 4:
-               		print("Yellow")
-       		elif color == 5:
-               		print("Red")
-       		elif color == 6:
-               		print("White")
-       		elif color == 7:
-               		print("Brown")
-	
 	def detectColor(self):
 		color = self.readColor()
 
@@ -70,35 +51,30 @@ class Controller:
 			return "White"
 		elif color == 7:
 			return "Brown"
-
+	def detectLightness(self):	
+		return self.colorSensor.value()
+	
 	def followTheLine(self):
 		turn = self.turnRight
 		turnTheOtherSide = self.turnLeft
-		
-		while True:		
-			if self.detectColor() == "Black":			
-				while True:
-					if self.detectColor() == "Black":
-						turn()
-					else:  # if detected color is different, robot must turn to the other side
-						while self.detectColor() != "Black": # if is white robot must turn till he won't achieve black color again
-							self.showDetectedColor()
-							turnTheOtherSide()
-				
-						# now we must change functions, in order to when robot just achieve black color he must turning to the
-						# same direction
-						self.showDetectedColor()
-						temp = turn 
-						turn = turnTheOtherSide
-						turnTheOtherSide = temp
-		
-		#flag = False  # we are on the left side
-		#while True:
 			
-			 
+		turn()
+		while True:
+			if self.detectLightness() > 10:	
+				# now we must change functions, in order to change engine
+				temp = turn 
+				turn = turnTheOtherSide
+				turnTheOtherSide = temp
+				
+				turn()
+
+				while self.detectLightness() > 6:
+					pass
+	def __del__(self):
+		self.stop()	
+		 
 			
 									
 robot = Controller()
 
 robot.followTheLine()
-
