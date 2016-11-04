981d7ca4f017333fec52ef11771641035cf219b2
Jakub Wieczorek <jwieczor@pantera.robotyka.ia.pw.edu.pl>
2016-11-04 12:02:21 +0100

    Follow the line done

--- Robot.py
+++ Robot.py
@@ -11,13 +11,15 @@ class Controller:
 		self.minLightness = 7.0
 		self.integral = 0
 		self.prevError = 0
+		self.frontColorSensor = ev3.ColorSensor('in2')
+
 	def goForward(self, leftSpeed = 100, rightSpeed = 100, time = None):
 		if time == None:	
 			self.rightMotor.run_forever(speed_sp = rightSpeed)
 			self.leftMotor.run_forever(speed_sp = leftSpeed)
 		else:	
-			self.leftMotor.run_timed(time_sp = 3000, speed_sp = leftSpeed)
-			self.rightMotor.run_timed(time_sp = 3000, speed_sp = rightSpeed)
+			self.leftMotor.run_timed(time_sp = time, speed_sp = leftSpeed)
+			self.rightMotor.run_timed(time_sp = time, speed_sp = rightSpeed)
 
 	def turnRight(self, speed = 100):
 		self.stop()
@@ -29,15 +31,15 @@ class Controller:
 		self.leftMotor.run_forever(speed_sp = 0)
 		self.rightMotor.run_forever(speed_sp = speed)
 			
-	def readColor(self):	
-		return self.colorSensor.color()
+	def readColor(self, sensor):	
+		return sensor.color()
 
 	def stop(self):
 		self.leftMotor.stop()
 		self.rightMotor.stop()
 
-	def detectColor(self):
-		color = self.readColor()
+	def detectColor(self, sensor):
+		color = self.readColor(sensor)
 
 		if color == 0:
 			return "None"
@@ -59,31 +61,24 @@ class Controller:
 		return self.colorSensor.value()
 	
 	def followTheLine(self):
-		speed = 200
-			
-		time = 0.0
+		speed = 150
 		
 		while True:
 			light = self.detectLightness()
-			"""
-			if light > 50 and time == 0.0:
-				time = Time.time()
-
-			if light < 25:
-				time = 0.0
-
-			if time - Time.time() > -5 and light > 50:
-				self.turnRight()
-				while self.detectLightness() > 26:
-					pass
-				time  = 0.0
-				light = self.detectLightness()
-			"""
 			self.goForward(speed * float((1 + self.turn(light))), speed * float((1 - self.turn(light))))
 	def __del__(self):
 		self.stop()	
 	
 	def turn(self, inputPos):
+		currentColor = self.detectColor(self.frontColorSensor)		
+
+		print(currentColor)
+
+		if currentColor == "Black":
+			return 0		
+		elif currentColor == "Red":
+			self.drag()	
+
 		a = 2.0 / (self.minLightness - self.maxLightness)
 		error = inputPos - (self.minLightness + self.maxLightness) / 2
 	
@@ -92,14 +87,29 @@ class Controller:
 		else:
 			self.integral = -pow(abs(self.integral), 0.5)
 	
-		print(self.integral)
-			
 		derivative = error - self.prevError
 		Ki = 0.01
 		Kd = 0.5
 		return a * (error + Ki * self.integral + Kd * derivative) 
+	
+	def drag(self):
+		self.goForward(100, 100)		
+
+		Time.sleep(1.2) # line between two wheels
 			
-									
+		self.goForward(100, -100) # first turn right
+		
+		Time.sleep(1.5) # block in fornt of robot
+		
+		if self.detectColor(self.frontColorSensor) == "White" and self.detectColor(self.colorSensor) == "White": # but if he turned to wrong side
+			self.goForward(-100, 100)
+			Time.sleep(2.85)
+		
+		self.goForward(0,0)
+
+		while True:
+			pass
+							
 robot = Controller()
 
 robot.followTheLine()
